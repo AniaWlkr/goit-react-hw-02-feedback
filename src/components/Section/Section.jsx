7e306d83abb01655d1efe35fef4d5ad1abbe73ef
Ania Walker <ania.wlkr@gmail.com>
2021-07-04 18:26:39 +0300

    hooks code refactoring

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,50 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import FeedbackOptions from '../FeedbackOptions/FeedbackOptions';
 import Statistics from '../Statistics/Statistics';
 import styles from './Section.module.css';
 
-class Section extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const Section = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setneutral] = useState(0);
+  const [bad, setbad] = useState(0);
+
+  const onButtonClick = btnName => {
+    switch (btnName) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setneutral(neutral + 1);
+        break;
+      case 'bad':
+        setbad(bad + 1);
+        break;
+      default:
+        console.log(`Sorry, we are out of ${btnName}.`);
+    }
   };
 
-  onButtonClick = btnName =>
-    this.setState(prevState => ({ [btnName]: prevState[btnName] + 1 }));
+  const totalStats = good + neutral + bad;
 
-  calcTotalStats = () =>
-    Object.values(this.state).reduce((acc, value) => acc + value, 0);
+  const calcPositives = () => {
+    if (!totalStats) return 0;
 
-  calcPositives = () => {
-    if (!this.calcTotalStats()) return 0;
-    const { good, neutral } = this.state;
-    const positives = Math.floor(
-      ((good + neutral) / this.calcTotalStats()) * 100,
-    );
+    const positives = Math.floor(((good + neutral) / totalStats) * 100);
     return positives;
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-
-    return (
-      <div className={styles.section}>
-        <FeedbackOptions
-          feedbackBtns={['good', 'neutral', 'bad']}
-          onLeaveFeedback={'feedbackCount'}
-          onButtonClick={this.onButtonClick}
-        />
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={this.calcTotalStats()}
-          positives={this.calcPositives()}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.section}>
+      <FeedbackOptions
+        feedbackBtns={['good', 'neutral', 'bad']}
+        onLeaveFeedback={'feedbackCount'}
+        onButtonClick={onButtonClick}
+      />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={totalStats}
+        positives={calcPositives()}
+      />
+    </div>
+  );
+};
 
 export default Section;
